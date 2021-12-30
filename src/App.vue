a84c69a651f4b57ce6d114228421f5348b66dfdb
MikolajPiecekMoliera <m.piecek@moliera2.com>
2021-12-30 13:00:28 +0100

    default styles, header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-	Hello world.
+	<div class="app-container">
+		<app-header></app-header>
+	</div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
 
 export default {
 	name: 'App',
 	components: {
-
+		AppHeader,
 	}
 }
 </script>
@@ -16,11 +19,23 @@ export default {
 :root {
 	font-family: 'Nunito Sans', sans-serif;
 
-	--normal: 14px;
-	--big: 16px;
+	--normal-font: 14px;
+	--bigger-font: 16px;
+
+	--lm-text: hsl(200, 15%, 8%);
+	--white: hsl(0, 0%, 100%);
 }
 
 *, * > * {
 	box-sizing: border-box;
+}
+
+.app-container {
+	color: var(--lm-text);
+	background: hsl(0, 0%, 98%);
+	min-height: 100vh;
+	width: 100vw;
+	font-size: var(--normal-font);
+	font-weight: 300;
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<template>
+	<header>
+		<h1>Where in the world?</h1>
+
+		<div class="toggle-theme">
+			<img src="../assets/moon-regular.svg">
+			<span>Dark Mode</span>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'AppHeader',
+}
+</script>
+
+<style scoped>
+	header {
+		display: flex;
+		justify-content: space-between;
+		padding: 25px 65px;
+		box-shadow: 0px -35px 66px 10px rgba(66, 68, 90, 0.64);
+		background: var(--white);
+	}
+
+	header h1 {
+		font-weight: 800;
+		font-size: var(--bigger-font);
+	}
+
+	header .toggle-theme {
+		font-weight: 600;
+		display: flex;
+		gap: 8px;
+	}
+
+	header .toggle-theme img {
+		height: 14px;
+		transform: rotate(-15deg);
+		margin-top: -1px;
+	}
+</style>
